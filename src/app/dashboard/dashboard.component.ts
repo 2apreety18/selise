5a45fe8f83ec0a01ecc04499233a4c9118150416
2apreety18 <2apreety@gmail.com>
2023-03-28 14:17:27 +0600

    localstorage add bookmark works

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,34 +11,45 @@ import { BookmarkService } from '../services/bookmark.service';
 export class DashboardComponent {
 
   reactiveForm = new FormGroup({
-    title: new FormControl('', Validators.required),
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
     url: new FormControl('', Validators.required),
     category: new FormControl('', Validators.required)
   });
 
-  bookmarks: Bookmark[] = [];
+  bookmarks!: Bookmark[];
   categories = this.data.getCategories();
   bookmark: Bookmark;
 
+
   constructor(private data: BookmarkService) {
-    this.data.getAllBookmarks().subscribe(data => {
-      console.log(data);
-      this.bookmarks = data;
-    });
+    // this.data.getAllBookmarks().subscribe(res => {
+    //   console.log(res);
+    //   this.bookmarks = res; 
+    // });
+
+    this.bookmarks = this.data.getAllBookmarks();
+
+    this.bookmarks = this.data.loadStorageBookmark();
   }
 
   onSubmit() {
     console.log(this.reactiveForm.value);
     const val = this.reactiveForm.value;
     const bookmarkRef: Bookmark = {
-      id: Math.floor(Math.random() * 10) + 1,
+      id: Math.floor(Math.random() * 100) + 1,
       title: val.title,
       url: val.url,
       category: val.category
      };
-    this.bookmarks.push(bookmarkRef);
+    // this.bookmarks.push(bookmarkRef);
+    this.data.addBookmark(bookmarkRef);
+
+
     // this.data.addBookmark(val.title!,val.url!,val.category!).subscribe(()=>{});
-    this.reactiveForm.reset();
+    // this.data.addBookmark(bookmarkRef);
+    // localStorage.setItem('bookmark_list', JSON.stringify(bookmarkRef));
+    // this.reactiveForm.reset();
+    // this.bookmark = JSON.parse(localStorage.getItem('bookmark_list'));
   }
 
   details(bookmark: Bookmark) {
--- src/app/services/bookmark.service.ts
+++ src/app/services/bookmark.service.ts
@@ -8,15 +8,60 @@ import { Bookmark } from '../interfaces/bookmark';
 })
 export class BookmarkService {
 
-  jsonURL = 'assets/bookmark.json'
-  bookmarks: Bookmark[] = [];
+  jsonURL = 'assets/bookmark.json';
+  storageName = 'bookmark_list';
+  bookmarks: Bookmark[] = [
+  //   {  
+  //     "id": 1,  
+  //     "title": "Javascript Tutorial",  
+  //     "url": "www.xyz.com",  
+  //     "category": "Category A"  
+  // },
+  // {  
+  //     "id": 2,  
+  //     "title": "Angular basics",  
+  //     "url": "www.xyz.com",  
+  //     "category": "Category A"  
+  // },
+  // {  
+  //     "id": 3,  
+  //     "title": "React introduction",  
+  //     "url": "www.xyz.com",  
+  //     "category": "Category A"  
+  // },
+  // {  
+  //     "id": 4,  
+  //     "title": "Best food in Dhaka To eat with...",  
+  //     "url": "www.xyz.com",  
+  //     "category": "Category B"  
+  // },
+  // {  
+  //     "id": 5,  
+  //     "title": "Burger vs Pizza",  
+  //     "url": "www.xyz.com",  
+  //     "category": "Category B"  
+  // }
+  ];
   bookmark!: any;
   categories: any[] = ["Category A","Category B"];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadStorageBookmark();
+  }
 
-  getAllBookmarks(): Observable<Bookmark[]> {
-    return this.http.get<Bookmark[]>(this.jsonURL);
+  // getAllBookmarks(): Observable<Bookmark[]> {
+  //   return this.http.get<Bookmark[]>(this.jsonURL);
+  // }
+  getAllBookmarks() {
+    return this.bookmarks;
+  }
+
+  saveStorageBookmarks() {
+    localStorage.setItem(this.storageName, JSON.stringify(this.bookmarks));
+  }
+
+  loadStorageBookmark(){
+    return JSON.parse(localStorage.getItem(this.storageName));
   }
 
   // getBookmarkDetails(id: number): Observable<Bookmark>{
@@ -31,13 +76,16 @@ export class BookmarkService {
     return this.categories;
   }
 
-  addBookmark (title: string, url: string, category: string) : Observable<Bookmark> {
-    const httpOptions = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
-    return this.http.post<Bookmark>(this.jsonURL,{ title, url, category}, httpOptions);
+  addBookmark (bookmark: Bookmark) {
+    // const httpOptions = {
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // };
+    // return this.http.post<Bookmark>(this.jsonURL,{ title, url, category}, httpOptions);
+    this.bookmarks.push(bookmark);
+    console.log(this.bookmarks);
+    this.saveStorageBookmarks();
   }
 
   // setBookmark(id: number){
